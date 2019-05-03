49c478df522444fd8b83fa27f1815ece97e7cc68
tcn741 <tianchenlko@126.com>
2019-05-03 21:21:57 +0800

    implement crud operations  by using bootstrap v4 and ajax in  asp.net mvc

---
+++ CRUDAjax/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace CRUDAjax.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    }
+}
